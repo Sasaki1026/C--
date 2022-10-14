6d56239cc04ca0ec81adbb37a4dd0b91a25b387d
Sasaki <kanonagi1026@gmail.com>
2022-10-14 10:42:58 +0800

    class to struct

--- HW2.cpp
+++ HW2.cpp
@@ -1,51 +1,74 @@
 #include<iostream>
 using namespace std;
-
-class node
+struct node
 {
-    public:
-        int coef;
-        int exp;
-        node* link = NULL;
+    int coef;
+    int exp;
+    node* link = NULL;
 };
 
-class linkedList
+
+struct linkedList
 {
-    public:
-        node* head = NULL;
-        node* reaf = NULL;
+    node* head = NULL;
+    node* rear = NULL;
+    int size = 0;
 };
+
+void push(linkedList q, int co, int exp)
+{
+    node *n = new node;
+    n->coef = co; n->exp = exp;
+    n->link = NULL;
+    if(q.head == NULL){
+        q.head = n;
+        q.rear = n;
+    }
+    else{
+        q.rear->link = n;
+        q.rear = n;
+    }
+    q.size++;
+}
+
+void dequene(linkedList q)
+{
+    if(q.head == NULL){
+        cout << "QueneNullException" << endl;
+    }
+    else{
+        q.head = q.head->link;
+        q.size--;
+    }
+}
+
+int topCoef(linkedList q)
+{
+    if(q.head == NULL){
+        cout << "QueneNullException" << endl;
+        return -10000;
+    }
+    else{
+        return q.head->coef;
+    }
+}
+
+int topExp(linkedList q)
+{
+    if(q.head == NULL){
+        cout << "QueneNullException" << endl;
+        return -10000;
+    }
+    else{
+        return q.head->exp;
+    }
+}
 
 int main()
 {
     linkedList L1, L2;
     int len;//length of poly
     cin >> len;
-    for(int i = 0; i < len; i++){
-        if(L1.head){
-            node tmp;
-            cin >> tmp.coef;
-            cin >> tmp.exp;
-            L1.reaf->link = &tmp;
-            L1.reaf = &tmp;            
-        }
-        else{
-            node tmp;
-            cin >> tmp.coef;
-            cin >> tmp.exp;
-            L1.head = &tmp;
-            L1.reaf = &tmp;
-        }
-        
-    }
 
-
-    for(int i = 0; i < 3; i++)
-    {
-        cout << L1.head->coef << " " << L1.head->exp << "\n";
-        L1.head = L1.head->link;
-    }
-
-    cout << L1.reaf->link->link;
     return 0;
 }
