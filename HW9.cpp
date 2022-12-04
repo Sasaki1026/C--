bbcc9dd9f6ab2ac540ba8b2622f370ffa5abc879
Sasaki <kanonagi1026@gmail.com>
2022-12-04 12:28:40 +0800

    1204 #1 AC

--- HW9.cpp
+++ HW9.cpp
@@ -1,67 +1,111 @@
-#include <iostream>
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
+
+typedef pair<int, int> edgePair;
+
+struct graph
+{
+    int V, E;
+    vector<pair<int, edgePair> > edges;
+
+    //constructor
+    graph(int V, int E)
+    {
+        this->V = V;
+        this->E = E;
+    }
+
+    void addEdge(int u, int v, int w)
+    {
+        edges.push_back({w, {u,v}});
+    }
+
+    int Kruskal();
+
+};
+
+struct disjoint//check disjoint sets of all vertices
+{
+    int *parent, *rank;
+    int n;
+
+    disjoint(int n)
+    {
+        this->n = n;
+        parent = new int[n+1];
+        rank = new int[n+1];
+
+        for (int i = 0; i < n; i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    int find(int a)
+    {
+        if(a != parent[a]){
+            parent[a] = find(parent[a]);
+        }
+        return parent[a];
+    }
+
+    void merge(int x, int y)
+    {
+        x = find(x), y = find(y);
+
+        if (rank[x] > rank[y]) parent[y] = x;
+
+        else parent[x] = y;
+  
+        if (rank[x] == rank[y]) rank[y]++;
+
+    }
+};
+
+int graph::Kruskal()
+{
+    int mstWeight = 0;
+
+    //sort edges increasing order
+    sort(edges.begin(), edges.end());
+
+    disjoint ds(V);
+
+    vector<pair<int, edgePair>>::iterator it;
+    for (it = edges.begin(); it != edges.end(); it++){
+        int u = it->second.first;
+        int v = it->second.second;
+
+        int setU = ds.find(u);
+        int setV = ds.find(v);
+
+        if (setU != setV){
+            mstWeight += it->first;
+            ds.merge(setU, setV);
+        }
+    }
+
+    return mstWeight;
+}
 
 int main()
 {
-
     int vertex, edge;
     cin >> vertex >> edge;
-    int MSTedge = 0;
+    graph g(vertex, edge);
 
-    // adjacency matrix
-    int adjM[vertex][vertex] = {0};
-
-    for (int i = 0; i < edge; i++)
-    {
-        int a, b, w;
-        cin >> a >> b >> w;
-        adjM[a][b] = w;
+    for (int i = 0; i < edge; i++){
+        int v1,v2,w;
+        cin >> v1 >> v2 >> w;
+        g.addEdge(v1,v2,w);
     }
 
-    // create an array to check visited vertex
-    bool visit[vertex];
-
-    // initialise the visit array to false
-    for (int i = 0; i < vertex; i++)
-    {
-        visit[i] = false;
-    }
-
-    visit[0] = true;
-
-    int totalWeight = 0;
-
-    while (MSTedge < vertex - 1)
-    {
-
-        int min = 1e9;
-        int nextPoint;
-
-        for (int i = 0; i < vertex; i++)
-        {
-            if (visit[i])
-            {
-                for (int j = 0; j < vertex; j++)
-                {
-                    if (!visit[j] && adjM[i][j])
-                    {
-                        if (min > adjM[i][j])
-                        {
-                            min = adjM[i][j];
-                            nextPoint = j;
-                        }
-                    }
-                }
-            }
-        }
-
-        totalWeight += min;
-
-        visit[nextPoint] = true;
-        MSTedge++;
-    }
-
-    cout << totalWeight << endl;
+    int ans = g.Kruskal();
+    cout << ans << endl;
 
     return 0;
+
 }
